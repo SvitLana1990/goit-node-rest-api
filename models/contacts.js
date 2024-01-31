cddaca161ecfecee72eeb9be2e92c00f60a3342a
SvitLana1990 <svitlana112@outlook.es>
2024-01-31 18:26:32 +0100

    fix_import-export

--- models/contacts.js
+++ models/contacts.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-const handleMongooseError = require("../midlevares/index");
+const { handleMongooseError } = require("../midlevares/index.js");
 
 const contactSchema = new Schema(
   {
